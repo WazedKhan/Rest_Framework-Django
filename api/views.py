b8bf5ede01a7e5e0913e819dcb4768a762d7a0d9
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-05-15 17:30:04 +0600

    changed json response to DRF response

--- api/views.py
+++ api/views.py
@@ -1,15 +1,17 @@
 import json
-from turtle import title
 from django.forms import model_to_dict
-from django.shortcuts import render
-from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    # DRF API view 
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data) # fields = ['id','title'] used to specify which fields to sent by default sent all the fields
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id','title','price','sale_price']) # fields = ['id','title'] used to specify which fields to sent by default sent all the fields
+    return Response(data)
     
