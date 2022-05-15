ce617e5fb1fce77d315dcf6fc2620485d56b5b1f
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-05-15 12:12:37 +0600

    serialize model data to json using model_to_dict

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 import json
+from turtle import title
+from django.forms import model_to_dict
 from django.shortcuts import render
 from django.http import JsonResponse
 
@@ -8,9 +10,6 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data) # fields = ['id','title'] used to specify which fields to sent by default sent all the fields
     return JsonResponse(data)
     
