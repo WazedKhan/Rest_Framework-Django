e622b4f752635edd98a7629fbc31226367cf2cd5
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-05-15 17:44:40 +0600

    added serializer for product model

--- api/views.py
+++ api/views.py
@@ -4,14 +4,15 @@ from django.forms import model_to_dict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from products.serializers import ProductSerializer
 from products.models import Product
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
     # DRF API view 
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id','title','price','sale_price']) # fields = ['id','title'] used to specify which fields to sent by default sent all the fields
+    if instance:
+        data = ProductSerializer(instance).data
     return Response(data)
     
---
+++ products/serializers.py
@@ -0,0 +1,11 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+
+from products.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'content', 'price', 'sale_price', 'get_discount']
