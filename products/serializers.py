82e34aa53bfa8a0b56770e83f511fe73bf5c0759
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-05-15 17:51:20 +0600

    overrided get_discount instance with discount

--- products/serializers.py
+++ products/serializers.py
@@ -6,6 +6,10 @@ from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    discount = serializers.SerializerMethodField(read_only = True)
     class Meta:
         model = Product
-        fields = ['id', 'title', 'content', 'price', 'sale_price', 'get_discount']
+        fields = ['id', 'title', 'content', 'price', 'sale_price', 'discount']
+
+    def get_discount(self, obj):
+        return obj.get_discount()
