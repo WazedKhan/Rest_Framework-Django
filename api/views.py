4ca33c2223504d2ada4c91a7811a535bc52feb70
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-05-15 11:42:37 +0600

    json response for get query

--- api/views.py
+++ api/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
+
+    print(request.GET)
+    print(request.POST)
     body = request.body # byte string of JSON data
     data = {}
     try:
         data = json.loads(body) #json.load converts json data into python dictionary
     except:
         pass
-    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
     return JsonResponse(data)
